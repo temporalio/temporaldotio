d66649bbc9ab2cf0d438280dda9fb5b4fc430c48
sw-yx <shawnthe1@gmail.com>
2020-10-15 06:32:54 +0800

    usecases tweak

--- components/UseCases.js
+++ components/UseCases.js
@@ -1,9 +1,16 @@
-// import Link from 'next/link'
 import React from 'react'
 
+/**
+ * 
+ * 
+ * 
+ * TODO: we use a homebrewed accordion here. probably not accessible.
+ * we originally tried https://github.com/springload/react-accessible-accordion/ but it caused jumps
+ * 
+ * 
+ */
 
 export default function UseCases() {
-  // const [index, setIndex] = React.useState(0)
   return (
     <div id="use-cases" className={`
     border-b border-spaceblack
@@ -11,7 +18,7 @@ export default function UseCases() {
     flex flex-col pt-16
     `}>
       <h1 className="container pl-4 mb-16 md:pl-16 text-60 leading-60 sm:text-144 sm:leading-144 uppercase text-spaceblack">Use Cases</h1>
-      <ul className="accordion border border-spaceblack mb-32 lg:container lg:ml-16">
+      <ul className="accordion border-t border-b lg:border border-spaceblack mb-32 lg:container lg:ml-16">
         <UCaseItem header="Microservice Orchestration" defaultState>
           <h2 className="font-bold mb-3">Temporal is a perfect fit for microservice orchestration scenarios.</h2>
           <p className="mb-3">It guarantees that workflow code eventually completes, has built-in support for exponential activity retries, and simplifies the coding of the compensation logic with native Saga pattern support.</p>
@@ -27,25 +34,34 @@ export default function UseCases() {
         </UCaseItem>
         <UCaseItem header="Resource Provisioning">
           <h2 className="font-bold mb-4">Provisioning resources depends on a series of potentially long-running operations with many possibilities for intermittent failures.</h2>
-          <p className="mb-3">While existing deployment tools support simple operations, many scenarios may still require a custom provisioning flow:</p>
-          <ul className="mb-3">
-            <li className="mb-2">Automatic infrastructure provisioning for a new customer in multi-tenant environments.</li>
+          <p className="mb-3">While existing deployment tools support simple operations, many scenarios may still require a custom provisioning flow.</p>
+          <p className="mb-3">Some provisioning operations may take dozens of minutes or even hours to complete. Ad-hoc solutions may fail in the middle and leave the system in an undefined state.</p>
+          <p className="mb-3">Temporal provides excellent building blocks for resource provisioning:</p>
+          <ul className="mb-3 list-disc ml-6 lg:w-700">
+            <li className="mb-2">Support for long-running operations</li>
+            <li className="mb-2">Polling</li>
+            <li className="mb-2">Responding to events</li>
+            <li className="mb-2">Automatic retries</li>
+            <li className="mb-2">Routing to specific hosts</li>
+            {/* <li className="mb-2">Automatic infrastructure provisioning for a new customer in multi-tenant environments.</li>
             <li className="mb-2">Particularly large deployments when tens or even hundreds of thousands of resources should be configured.</li>
             <li className="mb-2">Provisioning of custom resources that are not supported by off-the-shelf tools.</li>
-            <li className="mb-2">Complex configuration logic that is determined at deployment time.</li>
+            <li className="mb-2">Complex configuration logic that is determined at deployment time.</li> */}
           </ul>
-          <p className="mb-3">
+          <p className="mb-3">If a lengthy provisioning workflow fails in the middle, Temporal would handle the error and restart the flow at the right spot.</p>
+          {/* <p className="mb-3">
             It's beneficial to have a single workflow engine to manage all the various tasks: spinning up the cluster, long term monitoring, managing upgrades, database schema migrations, automated staged rollout of new features.
           </p>
           <p className="mb-3">Some provisioning operations may take dozens of minutes or even hours to complete. Ad-hoc solutions may fail in the middle and leave the system in an undefined state.</p>
           <p className="mb-3">Temporal workflows can express complex decision trees using a general-purpose programming language. Support for long-running operations, polling, responding to events, automatic retries are excellent building blocks for a robust provisioning flow. If a lengthy provisioning workflow fails in the middle, Temporal would handle the error and restart the flow at the right spot.</p>
           <p className="mb-3">Temporal can route activity execution to a specific process or host, which is useful for many provisioning scenarios.</p>
-          <p className="mb-3">Many resource management operations require locking to ensure that only one mutation is executed on any given resource at a time. Temporal provides a strong guarantee of uniqueness via the operation identifier. This primitive enables the implementation of locking behavior in a fault-tolerant and scalable manner.</p>
+          <p className="mb-3">Many resource management operations require locking to ensure that only one mutation is executed on any given resource at a time. Temporal provides a strong guarantee of uniqueness via the operation identifier. This primitive enables the implementation of locking behavior in a fault-tolerant and scalable manner.</p> */}
+
         </UCaseItem>
         <UCaseItem header="Monitoring">
           <h2 className="font-bold mb-4">Temporal provides guaranteed execution with exactly-once semantics with automatic retries.</h2>
           <p className="mb-3">Polling configuration can be as straightforward or sophisticated as needed:</p>
-          <ul className="mb-3">
+          <ul className="mb-3 list-disc ml-6 lg:w-700">
             <li className="mb-2">Workflows can run on a cron schedule with a single configuration setting.</li>
             <li className="mb-2">Alternatively, you can manually control the delays between intervals with `sleep` commands. For example, you can switch to more frequent executions in case of detected downtime.</li>
           </ul>
