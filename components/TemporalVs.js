aff3c4afb65751b61ed4944bc433c97ca1fc879f
Tihomir Surdilovic <tihomir@temporal.io>
2021-04-14 16:00:51 -0400

    Update components/TemporalVs.js
    Co-authored-by: Cully <cullywakelin@gmail.com>

--- components/TemporalVs.js
+++ components/TemporalVs.js
@@ -60,7 +60,7 @@ export default function TemporalVs() {
             capabilities provided by the underlying workflow language they use. Even though they
             promote the visualization aspect of workflow design, the actual execution semantics are
             expressed as a mix of programming and expression languages which are enforced by the
-            specific runtime implementation. Temporal does not suffer from these type of
+            specific runtime implementation. Temporal does not suffer from these types of
             restrictions allowing you to fully define your workflows using a programming language.
             This provides a high level of flexibility not only with defining your workflow logic but
             also testing and debugging in the development environment of your choice.
