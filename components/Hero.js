bde4d50cdf398a55ee9a97f02ebfb8f3fdcabf7c
sw-yx <shawnthe1@gmail.com>
2021-07-01 15:34:04 -0700

    add samples

--- components/Hero.js
+++ components/Hero.js
@@ -3,31 +3,23 @@ import { Nav } from './Nav';
 import DirectionalControl from './DirectionalControl';
 import OrbitalCases from './OrbitalCases';
 import React from 'react';
-
-const allSites = [
-  {
-    src: '/logos/logo-Box2.png',
-    label: 'Box logo',
-    url: 'https://docs.temporal.io/blog/Temporal-a-central-brain-for-Box'
-  },
-  {
-    src: '/logos/logo-Checkr.png',
-    label: 'Checkr logo',
-    url: 'https://docs.temporal.io/blog/how-temporal-simplified-checkr-workflows'
-  },
-  {
-    src: '/logos/logo-Coinbase.svg',
-    label: 'Coinbase logo',
-    url: 'https://docs.temporal.io/blog/reliable-crypto-transactions-at-coinbase'
-  },
-  {
-    src: '/logos/logo-Descript.png',
-    label: 'Descript logo',
-    url: 'https://docs.temporal.io/blog/descript-case-study'
-  }
-];
+import * as HeroSamples from './HeroSamples';
 
 export default function Hero() {
+  const [lang, setLang] = useLocalStorage('langpref', 'Go');
+  const dynamicCodeSample = {
+    Go: HeroSamples.Go,
+    Java: HeroSamples.Java,
+    PHP: HeroSamples.PHP,
+    Node: HeroSamples.Node
+  }[lang] || (
+    <div className="text-lg">
+      Python, Ruby, dotNet, and other SDKs are planned for next year.{' '}
+      <a className="text-blue-500 no-underline hover:underline" href="/careers">
+        Join us!
+      </a>
+    </div>
+  );
   return (
     <div className=" border-b border-white">
       {/* <Banner /> */}
@@ -37,23 +29,37 @@ export default function Hero() {
     min-h-screen max-h-[1080px] container mx-auto flex flex-col justify-between p-4 sm:p-4
     `}>
         <Nav />
-        <div>
-          <h1 className="text-60 leading-60 mb-8 lg:text-144 lg:leading-144 uppercase lg:w-800">
-            Build Invincible Apps
-          </h1>
-          <p className="text-2xl lg:w-700 mb-8">
-            {/* Temporal is the <Bold>open source</Bold> runtime for running <Bold>mission critical</Bold> code atop <Bold>unreliable, distributed</Bold> services at any scale. */}
-            {/* Highly reliable, globally scalable microservice orchestration for mission-critical applications */}
-            The platform for running mission critical code at&nbsp;
-            <OrbitalCases />
-            scale.
-          </p>
+        <div className="flex flex-col">
+          <div className="flex-auto">
+            <h1 className="text-60 leading-60 mb-8 lg:text-8xl lg:leading-8xl uppercase lg:w-800">
+              Build Invincible Apps
+            </h1>
+            <p className="text-2xl mb-8">
+              {/* Temporal is the <Bold>open source</Bold> runtime for running <Bold>mission critical</Bold> code atop <Bold>unreliable, distributed</Bold> services at any scale. */}
+              {/* Highly reliable, globally scalable microservice orchestration for mission-critical applications */}
+              The Open Source Runtime for
+              <select
+                className="bg-gray-800 active:bg-gray-500 mx-2"
+                value={lang}
+                onChange={(e) => setLang(e.target.value)}>
+                <option value="Go">Go</option>
+                <option value="Java">Java</option>
+                <option value="PHP">PHP</option>
+                <option value="Node">Node.js</option>
+                <option value="Other">any</option>
+              </select>
+              service orchestration at&nbsp;
+              <OrbitalCases />
+              scale.
+            </p>
+          </div>
+          {dynamicCodeSample}
         </div>
         <div className="flex flex-col lg:flex-row justify-between mb-8 sm:mb-8 items-center">
           <div className="flex flex-col gap-4 lg:flex-row lg:text-xl">
             <DirectionalControl href="#explain-temporal">2 Minute Intro</DirectionalControl>
             <DirectionalControl secondary href="https://docs.temporal.io/application-development">
-              Get Started
+              Run Hello World
             </DirectionalControl>
           </div>
           <iframe
@@ -68,4 +74,21 @@ export default function Hero() {
       </section>
     </div>
   );
+}
+
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = React.useState(initialValue);
+  React.useEffect(() => {
+    // Get from local storage by key
+    const item = typeof window !== 'undefined' ? window.localStorage.getItem(key) : false;
+    // Parse stored json or if none return initialValue
+    if (item) setStoredValue(JSON.parse(item));
+  }, [setStoredValue]);
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+  return [storedValue, setValue];
 }
---
+++ components/HeroSamples.js
@@ -0,0 +1,116 @@
+export const Node = (
+  <pre className="p-4 overflow-scroll" style="background-color: #292D3E">
+    <code>
+      <span style="display:block"></span>
+      <span style="display:block">
+        <span style="color: #676E95; font-style: italic">
+          // define interactionSignal handler in your favorite framework
+        </span>
+      </span>
+      <span style="display:block">
+        <span style="color: #89DDFF; font-style: italic">export</span>
+        <span style="color: #A6ACCD"> </span>
+        <span style="color: #C792EA">async</span>
+        <span style="color: #A6ACCD"> </span>
+        <span style="color: #C792EA">function</span>
+        <span style="color: #A6ACCD"> </span>
+        <span style="color: #82AAFF">mainWorkflow</span>
+        <span style="color: #89DDFF">(</span>
+        <span style="color: #A6ACCD">userId</span>
+        <span style="color: #89DDFF">:</span>
+        <span style="color: #A6ACCD"> </span>
+        <span style="color: #FFCB6B">string</span>
+        <span style="color: #89DDFF">)</span>
+        <span style="color: #A6ACCD"> </span>
+        <span style="color: #89DDFF">{'{'}</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #F07178"> </span>
+        <span style="color: #C792EA">const</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #A6ACCD">userInteracted</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #89DDFF">=</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #89DDFF; font-style: italic">await</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #FFCB6B">Promise</span>
+        <span style="color: #89DDFF">.</span>
+        <span style="color: #82AAFF">race</span>
+        <span style="color: #F07178">([</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #F07178"> </span>
+        <span style="color: #A6ACCD">interactionSignal</span>
+        <span style="color: #89DDFF">,</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #676E95; font-style: italic">// receive signals from users</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #F07178"> </span>
+        <span style="color: #82AAFF">sleep</span>
+        <span style="color: #F07178">(</span>
+        <span style="color: #F78C6C">30</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #89DDFF">*</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #A6ACCD">DAY</span>
+        <span style="color: #F07178">)] </span>
+        <span style="color: #676E95; font-style: italic">// sleep for 30 days!</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #F07178"> )</span>
+        <span style="color: #89DDFF">;</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #F07178"> </span>
+        <span style="color: #89DDFF; font-style: italic">if</span>
+        <span style="color: #F07178"> (</span>
+        <span style="color: #89DDFF">!</span>
+        <span style="color: #A6ACCD">userInteracted</span>
+        <span style="color: #F07178">) </span>
+        <span style="color: #89DDFF">{'{'}</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #F07178"> </span>
+        <span style="color: #89DDFF; font-style: italic">await</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #82AAFF">sendEmailActivity</span>
+        <span style="color: #F07178">(</span>
+        <span style="color: #A6ACCD">userId</span>
+        <span style="color: #F07178">)</span>
+        <span style="color: #89DDFF">;</span>
+        <span style="color: #F07178"> </span>
+        <span style="color: #676E95; font-style: italic">// timeouts + retries!</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #F07178"> </span>
+        <span style="color: #89DDFF">{'}'}</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #89DDFF">{'}'}</span>
+      </span>
+      <span style="display:block">
+        <span style="color: #676E95; font-style: italic">
+          // Write Tests + Scale Horizontally + Version Workflows + Encrypt Data + ...{' '}
+        </span>
+      </span>
+    </code>
+  </pre>
+);
+
+export const Go = (
+  <pre className="p-4 overflow-scroll" style="background-color: #292D3E">
+    <code>GOLANG CODE HERE</code>
+  </pre>
+);
+export const Java = (
+  <pre className="p-4 overflow-scroll" style="background-color: #292D3E">
+    <code>JAVA CODE HERE</code>
+  </pre>
+);
+export const PHP = (
+  <pre className="p-4 overflow-scroll" style="background-color: #292D3E">
+    <code>PHP CODE HERE</code>
+  </pre>
+);
