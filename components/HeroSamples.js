47165471bbee2da86009768a16575d629d313fb9
sw-yx <shawnthe1@gmail.com>
2021-07-02 07:59:43 -0700

    scroll behavior

--- components/HeroSamples.js
+++ components/HeroSamples.js
@@ -1,6 +1,6 @@
 export const Node = (
-  <pre className="p-4 overflow-scroll font-mono mb-4" style="background-color: #292D3E">
-    <code><span style="display:block"></span>
+  <pre className="p-4 overflow-hidden font-mono mb-4" style="background-color: #292D3E">
+    <code className="overflow-x-auto"><span style="display:block"></span>
 <span style="display:block"><span style="color: #676E95; font-style: italic">// define interactionSignal handler in your favorite framework</span></span>
 <span style="display:block"><span style="color: #89DDFF; font-style: italic">export</span><span style="color: #A6ACCD"> </span><span style="color: #C792EA">async</span><span style="color: #A6ACCD"> </span><span style="color: #C792EA">function</span><span style="color: #A6ACCD"> </span><span style="color: #82AAFF">mainWorkflow</span><span style="color: #89DDFF">(</span><span style="color: #A6ACCD">userId</span><span style="color: #89DDFF">)</span><span style="color: #A6ACCD"> </span><span style="color: #89DDFF">{"{"}</span></span>
 <span style="display:block"><span style="color: #F07178">  </span><span style="color: #C792EA">const</span><span style="color: #F07178"> </span><span style="color: #A6ACCD">userInteracted</span><span style="color: #F07178"> </span><span style="color: #89DDFF">=</span><span style="color: #F07178"> </span><span style="color: #89DDFF; font-style: italic">await</span><span style="color: #F07178"> </span><span style="color: #FFCB6B">Promise</span><span style="color: #89DDFF">.</span><span style="color: #82AAFF">race</span><span style="color: #F07178">([</span></span>
@@ -15,17 +15,17 @@ export const Node = (
 );
 
 export const Go = (
-  <pre className="p-4 overflow-scroll" style="background-color: #292D3E">
+  <pre className="p-4 overflow-hidden " style="background-color: #292D3E">
     <code>GOLANG CODE HERE</code>
   </pre>
 );
 export const Java = (
-  <pre className="p-4 overflow-scroll" style="background-color: #292D3E">
+  <pre className="p-4 overflow-hidden " style="background-color: #292D3E">
     <code>JAVA CODE HERE</code>
   </pre>
 );
 export const PHP = (
-  <pre className="p-4 overflow-scroll" style="background-color: #292D3E">
+  <pre className="p-4 overflow-hidden " style="background-color: #292D3E">
     <code>PHP CODE HERE</code>
   </pre>
 );
