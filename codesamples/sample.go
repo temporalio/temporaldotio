a5325df58f92f83844594579abe525cd805eae11
swyx <shawnthe1@gmail.com>
2021-07-06 16:06:45 -0700

    Apply suggestions from code review
    Co-authored-by: Roey Berman <roey.berman@gmail.com>

--- codesamples/sample.go
+++ codesamples/sample.go
@@ -1,9 +1,9 @@
 func mainWorkflow(ctx workflow.Context, userId string) error {
 	// ...
 	intervals := [3]int{1, 7, 30}
-	for _, i := range intervals {
+	for _, interval := range intervals {
 		time.Sleep(i * time.Day)                                           // sleep for days!
-		_ = workflow.ExecuteActivity(ctx, SendEmailActivity).Get(ctx, nil) // timeouts and retries!
+		_ = workflow.ExecuteActivity(ctx, SendUserRetentionEmail, userId, interval).Get(ctx, nil) // timeouts and retries!
 	}
-	// we can also signal or cancel the workflow as needed
+	// we can signal or cancel the workflow if the user does an action
 }
--- codesamples/sample.js
+++ codesamples/sample.js
@@ -2,7 +2,7 @@ async function main(userId) {
   const intervals = [1, 7, 30]; // Nag user until giving up
   for (const interval of intervals) {
     await sleep(interval * DAYS);
-    await sendEmailActivity(interval, userId); // automatic timeouts + retries
+    await activities.sendUserRetentionEmail(interval, userId); // automatic timeouts + retries
   }
   // we can also signal or cancel the workflow as needed
 }
