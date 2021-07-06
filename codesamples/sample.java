c9a4c35ec7de326424db08856fd156f7b1360877
swyx <shawnthe1@gmail.com>
2021-07-06 13:10:57 -0700

    remove codesamples and fix java sample

--- codesamples/sample.java
+++ codesamples/sample.java
@@ -1,12 +1,6 @@
-public class SampleWorkflow {
-  public static void main(String[] args) {
-    // ...
-    WorkflowClient.start(workflow::initWorkflow);
-    int intervals[] = { 1, 7, 30 };
-    for (int i : intervals) {   
-      Workflow.sleep(Duration.ofDays(i)); // sleep for days!
-      workflow.sendEmail(userId) // automatic timeouts + retries!
-    }
-    // we can also signal or cancel the workflow as needed
-  }
+int intervals[] = { 1, 7, 30 };
+for (int i : intervals) {   
+  Workflow.sleep(Duration.ofDays(i)); // sleep for days!
+  workflow.sendEmail(userId) // automatic timeouts + retries!
 }
+// we can also signal or cancel the workflow as needed
